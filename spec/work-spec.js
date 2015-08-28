59e09cf374c0f13e8f393ec9bd8fe60fd3304a83
carolineF <1138483832@qq.com>
2015-08-28 19:04:46 +0800

    eliminate duplicate code in worker

--- spec/work-spec.js
+++ spec/work-spec.js
@@ -5,20 +5,22 @@ var Person = require('../src/person');
 
 describe('Student', function() {
 
+    var worker;
+    beforeEach(function() {
+        worker = new Worker('Tom', 21);
+    });
+
     it('should have name and age', function() {
-        var worker = new Worker('Tom', 21);
         expect(worker.name).toBe('Tom');
         expect(worker.age).toBe(21);
     });
 
     it('should is a instance of Person', function() {
-        var worker = new Worker('Tom', 21);
         expect(worker instanceof Person).toBeTruthy();
     });
 
     describe('#introduce()', function() {
         it('should return My name is Tom. I am 21 years old. I am a Worker. I have a job.', function() {
-            var worker = new Worker("Tom", 21);
             expect(worker.introduce()).toBe('My name is Tom. I am 21 years old. I am a Worker. I have a job.');
         });
     });
