ea8b81cf030ae3b5f0b88eaf83ebcc37f6346ee1
carolineF <1138483832@qq.com>
2015-08-28 19:01:58 +0800

    eliminate duplicate code in student

--- spec/student-spec.js
+++ spec/student-spec.js
@@ -5,17 +5,21 @@ var Class = require('../src/class');
 
 describe('Student', function() {
 
-  it('should have name and age and class', function() {
-      var student = new Student('Tom', 21, 'Class 2');
+    var student;
+
+    beforeEach(function() {
+        var klass = new Class(2);
+        student = new Student("Tom", 21, klass);
+    });
+
+  it('should have name and age and class object', function() {
       expect(student.name).toBe('Tom');
       expect(student.age).toBe(21);
-      expect(student.class).toBe('Class 2');
+      expect(student.class).toEqual(new Class(2));
     });
 
     describe('#introduce()', function() {
       it('should return My name is Tom. I am 21 years old. I am a Student. I am at Class 2.', function() {
-          var klass = new Class(2);
-        var student = new Student("Tom", 21, klass);
         expect(student.introduce()).toBe('My name is Tom. I am 21 years old. I am a Student. I am at Class 2.');
       });
     });
